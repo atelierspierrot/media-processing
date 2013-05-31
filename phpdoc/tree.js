0cfd3b3adb9e4c87801a6490c908773d032cf77c
Piero Wbmstr <me@e-piwi.fr>
2013-05-31 21:21:33 +0200

    Documentation

---
+++ phpdoc/tree.js
@@ -0,0 +1,13 @@
+var tree = [["[Global Namespace]",,"",
+                [["SplClassLoader","SplClassLoader.html","",
+                [        ]]        ]],["MediaProcessing","MediaProcessing.html","",
+                [["ImageFilter","MediaProcessing\/ImageFilter.html","",
+                [["Filter","MediaProcessing\/ImageFilter\/Filter.html","",
+                [["Resize","MediaProcessing\/ImageFilter\/Filter\/Resize.html"," < AbstractFilter",
+                [        ]]        ]],["ImageFilter","MediaProcessing\/ImageFilter\/ImageFilter.html","",
+                [        ]]        ]],["AbstractFilter","MediaProcessing\/AbstractFilter.html","",
+                [        ]],["FilterInterface","MediaProcessing\/FilterInterface.html","",
+                [        ]],["MediaFile","MediaProcessing\/MediaFile.html"," < WebFileInfo",
+                [        ]],["MediaProcessor","MediaProcessing\/MediaProcessor.html","",
+                [        ]]        ]]        ]
+
