3a41d434bb6f773b710eea0d9add3b96f8b307aa
Piero Wbmstr <me@e-piwi.fr>
2014-06-14 09:55:25 +0200

    new piwi github username + 2014 copyleft

--- src/MediaProcessing/ImageFilter/ImageFilter.php
+++ src/MediaProcessing/ImageFilter/ImageFilter.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Media Processing package of Les Ateliers Pierrot
- * Copyleft (c) 2013 Pierre Cassat and contributors
+ * Copyleft (c) 2013-2014 Pierre Cassat and contributors
  * <www.ateliers-pierrot.fr> - <contact@ateliers-pierrot.fr>
  * License GPL-3.0 <http://www.opensource.org/licenses/gpl-3.0.html>
- * Sources <https://github.com/atelierspierrot/media-processing>
+ * Sources <http://github.com/atelierspierrot/media-processing>
  */
 
 namespace MediaProcessing\ImageFilter;
